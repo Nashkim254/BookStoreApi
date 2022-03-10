836565e493dd6e9dd90a66cb86221a7e27de6980
programming6131 <kimutainashon@gmail.com>
2022-03-10 15:47:37 +0300

    routes

--- routes/bookstore_routes.go
+++ routes/bookstore_routes.go
@@ -1,1 +1,15 @@
 package routes
+
+import (
+	"github.com/gorrilla/mux"
+	"github.com/programming6131/BookStoreApi/controller"
+)
+
+var RegisterBookRoutes = func(router *mux.Router) {
+
+	router.HandleFunc("/book", controller.CreateBook).Methods("POST")
+	router.HandleFunc("/book", controller.GetteBook).Methods("GET")
+	router.HandleFunc("/book/{id}", controller.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{id}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{id}", controller.DeleteBook).Methods("DELETE")
+}
