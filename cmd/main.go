01ec4f0b2d68761c414d5ed41e8782d1c5f84399
programming6131 <kimutainashon@gmail.com>
2022-03-11 16:34:10 +0300

    endgame

--- cmd/main.go
+++ cmd/main.go
@@ -13,5 +13,13 @@ func main() {
 	r := mux.NewRouter()
 	routes.RegisterBookRoutes(r)
 	http.Handle("/", r)
-	log.Fatal(http.ListenAndServe("localhost:8080", r))
+	srv := &http.Server{
+		Handler: r,
+		Addr:    "127.0.0.1:8000",
+		// Good practice: enforce timeouts for servers you create!
+		WriteTimeout: 15 * time.Second,
+		ReadTimeout:  15 * time.Second,
+	}
+
+	log.Fatal(srv.ListenAndServe())
 }
--- routes/bookstore_routes.go
+++ routes/bookstore_routes.go
@@ -1,15 +1,15 @@
 package routes
 
 import (
-	"github.com/gorrilla/mux"
-	"github.com/programming6131/BookStoreApi/controller"
+	"github.com/gorilla/mux"
+	"github.com/programming6131/BookStoreApi/pkg/controller"
 )
 
 var RegisterBookRoutes = func(router *mux.Router) {
 
-	router.HandleFunc("/book", controller.CreateBook).Methods("POST")
-	router.HandleFunc("/book", controller.GetteBook).Methods("GET")
-	router.HandleFunc("/book/{id}", controller.GetBookById).Methods("GET")
-	router.HandleFunc("/book/{id}", controller.UpdateBook).Methods("PUT")
-	router.HandleFunc("/book/{id}", controller.DeleteBook).Methods("DELETE")
+	router.HandleFunc("/book/", controller.CreateBook).Methods("POST")
+	router.HandleFunc("/book/", controller.GetBook).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.GetBookById).Methods("GET")
+	router.HandleFunc("/book/{bookId}", controller.UpdateBook).Methods("PUT")
+	router.HandleFunc("/book/{bookId}", controller.DeleteBook).Methods("DELETE")
 }
