19a02b4192ae6b7db1acb33a9cfbc9618935736b
programming6131 <kimutainashon@gmail.com>
2022-03-11 09:59:36 +0300

    database functions

--- models/models.go
+++ models/models.go
@@ -20,3 +20,29 @@ func init()  {
 	db =config.GetDB()
 	db.AutoMigrate(&Book{})
 }
+
+func (b *Book)  CreateBook() *Book{
+	db.NewRecord(b)
+	db.Create(&b)
+	return b
+}
+
+func GetAllBooks() []Book {
+	var Books []Book
+	db.Find(&Books)
+	return Books
+}
+
+func GetBookById(Id int64) (*Book, *gorm.DB) {
+	var getBook Book
+	db:=db.where("ID = ?", Id).Find(&getBook)
+
+	return &getBook, db
+}
+
+
+func DeleteBook(ID int64) Book {
+	var book Book
+	db.where("ID = ?", ID).Delete(book)
+	return book
+}
